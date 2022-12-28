6c9bb1ccf9a7b09603fdcc4147485b71b7b79589
Nicolau Gomes <115539407+GomesNikolau@users.noreply.github.com>
2022-12-28 12:01:23 +0300

    atualizei java

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+// menu.addEventListener("click", function () {
+//     navbar.classList.toggle('navbar');
+// });
+// window.onscroll = () => {
+//     navbar.classList.remove('navbar');
+// };
+
 menu.addEventListener("scroll", function () {
     navbar.classList.toggle("sticky",window.scrollY > 0);
 });
@@ -14,3 +21,6 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('navbar');
 };
+
+// link video no youtube
+// https://www.youtube.com/watch?v=lk99tsBF9Kw
